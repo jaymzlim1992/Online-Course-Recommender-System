5bebd159ded7a01d213588230b640f0345edb54d
JMLim <jmlim1992@gmail.com>
2021-10-25 01:37:39 +0800

    Added in open browser automation Changed the 'more info' button for course to open a new browser tab when clicked instead of navigating to the page in current browser

--- SystemCode/run.py
+++ SystemCode/run.py
@@ -1,4 +1,12 @@
 from frontend import app
+from threading import Timer
+import os
+import webbrowser
+
+def open_browser():
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        webbrowser.open_new('http://localhost:3000')
 
 if __name__ == '__main__':
+    Timer(1, open_browser).start();
     app.run(debug=True, host='0.0.0.0', port=3000)
